ea1df72dbb5b7d21a64ce123b08e1304e8c368a5
Edoardo <edoardoderosaa@gmail.com>
2024-06-26 14:22:06 +0200

    ProfilePage improvements

--- src/components/ProfilePage/ProfileHeader.tsx
+++ src/components/ProfilePage/ProfileHeader.tsx
@@ -1,10 +1,19 @@
 import ai4 from "../../assets/random_imgs/ai4.jpg";
+import ai3 from "../../assets/random_imgs/ai3.jpeg";
 
 export default function ProfileHeader() {
   return (
-    <div className="flex flex-col items-center p-6">
-      <img src={ai4} alt="Profile Picture" className="rounded-full w-24 h-24" />
-      <h2 className="mt-4 font-sans">Username</h2>
+    <div>
+      <img src={ai3} alt="Pofile Cover" className="w-full h-40 object-cover" />
+
+      <div className="flex flex-col items-center text-white -mt-12">
+        <img
+          src={ai4}
+          alt="Profile Picture"
+          className="rounded-full w-24 h-24 border-4 border-white"
+        />
+        <h2 className="mt-4 text-center font-sans">Username</h2>
+      </div>
     </div>
   );
 }
--- src/components/ProfilePage/ProfileStats.tsx
+++ src/components/ProfilePage/ProfileStats.tsx
@@ -1,6 +1,6 @@
 export default function ProfileStats() {
   return (
-    <div className="flex justify-around font-sans p-4">
+    <div className="flex justify-around font-sans p-4 text-white">
       <div className="text-center">
         <h3>Amici</h3>
         <p>100</p>
