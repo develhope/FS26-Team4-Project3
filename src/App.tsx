704e02ff2a7b50fe328810848201ef1aa9fa296f
Edoardo <edoardoderosaa@gmail.com>
2024-06-26 15:19:44 +0200

    ProfilePage commented in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,13 @@ import Login2 from "./components/Login/Login2";
 import Login3 from "./components/Login/Login3";
 import CardFeed from "./components/Cards/CardFeed";
 import { PostMain } from "./components/Posts/PostMain";
-import { FeedProfile } from "./components/ProfilePage/FeedProfile";
 import MessageCenter from "./components/Messages/MessageCenter";
 import { Chat } from "./components/Messages/Chat";
 import SwitchProfile from "./components/Login/SwitchProfile";
+import ProfileHeader from "./components/ProfilePage/ProfileHeader";
+import ProfileStats from "./components/ProfilePage/ProfileStats";
+import ProfileActions from "./components/ProfilePage/ProfileActions";
+import { ProfileGallery } from "./components/ProfilePage/ProfileGallery";
 
 function App() {
   return (
@@ -22,7 +25,10 @@ function App() {
       */}
       {/* <MessageCenter />
       <Chat /> */}
-      <SwitchProfile />
+      <ProfileHeader />
+      <ProfileStats />
+      <ProfileActions />
+      <ProfileGallery />
     </div>
   );
 }
