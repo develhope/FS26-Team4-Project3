c29b89c5a45ca30974bf3b04e37b7132bd82af6b
Edoardo <edoardoderosaa@gmail.com>
2024-07-01 16:23:56 +0200

    Added routes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,30 @@
-import Login from "./components/Login/Login1";
-import SettingsMainPage from "./components/Settings/SettingsMainPage";
 import "../src/components/Login/Login.css";
-import "./index.css";
+import CardFeed from "./components/Cards/CardFeed";
 import Login1 from "./components/Login/Login1";
 import Login2 from "./components/Login/Login2";
 import Login3 from "./components/Login/Login3";
-import CardFeed from "./components/Cards/CardFeed";
-import { PostMain } from "./components/Posts/PostMain";
-import MessageCenter from "./components/Messages/MessageCenter";
-import { Chat } from "./components/Messages/Chat";
-import SwitchProfile from "./components/Login/SwitchProfile";
-import ProfileHeader from "./components/ProfilePage/ProfileHeader";
-import ProfileStats from "./components/ProfilePage/ProfileStats";
-import ProfileActions from "./components/ProfilePage/ProfileActions";
-import { ProfileGallery } from "./components/ProfilePage/ProfileGallery";
-import AccountManagement from "./components/Settings/AccountManagement";
-import Notification from "./components/Settings/Notification";
-import PravacyAccount from "./components/Settings/PravicyAccount";
-import SettingFriends from "./components/Settings/SettingFriends";
-import SettingInteractions from "./components/Settings/SettingInteractions";
-import SettingLanguage from "./components/Settings/SettingLanguage";
-import SettingHelp from "./components/Settings/SettingHelp";
-import FooterNavbar from "./components/Navbars/FooterNavbar";
-import ProfileHeaderNavbar from "./components/Navbars/ProfileHeaderNavbar";
-import FeedHeaderNavbar from "./components/Navbars/FeedHeaderNavbar";
-import MessagesHeaderNavbar from "./components/Navbars/MessagesHeaderNavbar";
-import ChatHeaderNavbar from "./components/Navbars/ChatHeaderNavbar";
-import SettingsFooterNavbar from "./components/Navbars/SettingsFooterNavbar";
-import SettingsHeaderNavbar from "./components/Navbars/SettingsHeaderNavbar";
-import EditorHeaderNavbar from "./components/Navbars/EditorHeaderNavbar";
+import LoginA from "./components/Login/LoginA";
+import ProfilePage from "./components/ProfilePage/ProfilePage";
+import SettingsMainPage from "./components/Settings/SettingsMainPage";
+import Splash from "./components/Splash";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
 function App() {
   return (
-    <div>
-      {/*
-      <FeedHeaderNavbar />
-      <CardFeed />
-      <PostMain />
-      <FeedProfile />
-      */}
-      {/* <MessageCenter />
-      <Chat /> */}
-      {/*  <ProfileHeader />
-      <ProfileStats />
-      <ProfileActions />
-      <ProfileGallery /> */}
-     {/*  <Setting /> */}
-      {/* <AccountManagement /> */}
-      {/* <Notification /> */}
-      {/* <PravacyAccount /> */}
-      {/* <FooterNavbar /> */}
-      {/*<MessagesHeaderNavbar />*/}
-      {/*<ChatHeaderNavbar />
-      <MessageCenter />
-      <Chat />
-      <FooterNavbar />*/}
-      {/*<ProfileHeaderNavbar />
-      <ProfileHeader />
-      <ProfileStats />
-      <ProfileActions />
-      <ProfileGallery />
-      <FooterNavbar />*/}
-      {/*<SettingsHeaderNavbar />*/}
-      {/* <EditorHeaderNavbar />
-      <SettingsMainPage />
-      <SettingsFooterNavbar /> */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Splash />} />
+        <Route path="/login1" element={<Login1 />} />
+        <Route path="/login2" element={<Login2 />} />
+        <Route path="/login3" element={<Login3 />} />
+        <Route path="/LoginA" element={<LoginA />} />
+        <Route path="/cardfeed" element={<CardFeed />} />
+        <Route path="/profilepage" element={<ProfilePage />} />
+        <Route path="/settingsmainpage" element={<SettingsMainPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
