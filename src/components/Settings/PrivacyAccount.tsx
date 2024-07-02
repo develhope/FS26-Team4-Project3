3489dfb5e3cede277ffa7cb27818cf5d9993507f
Edoardo <edoardoderosaa@gmail.com>
2024-07-02 17:33:10 +0200

    Implemented routes and links for settings page

--- src/components/Settings/PrivacyAccount.tsx
+++ src/components/Settings/PrivacyAccount.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function PravacyAccount() {
+export default function PrivacyAccount() {
   const [isPrivate, setIsPrivate] = useState(false);
   const [isPrivateImage, setIsPrivateImage] = useState(false);
 
