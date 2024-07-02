040a9c5273dbef75255149103470423c990e6567
_edera_ <fedecprinzi@gmail.com>
2024-07-02 11:06:57 +0200

    adding-states-MainPost_Federica

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,30 @@ import Login1 from "./components/Login/Login1";
 import Login2 from "./components/Login/Login2";
 import Login3 from "./components/Login/Login3";
 import LoginA from "./components/Login/LoginA";
+import { PostMain } from "./components/Posts/PostMain";
 import ProfilePage from "./components/ProfilePage/ProfilePage";
 import SettingsMainPage from "./components/Settings/SettingsMainPage";
 import Splash from "./components/Splash";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Splash />} />
-        <Route path="/login1" element={<Login1 />} />
-        <Route path="/login2" element={<Login2 />} />
-        <Route path="/login3" element={<Login3 />} />
-        <Route path="/LoginA" element={<LoginA />} />
-        <Route path="/feed" element={<Feed />} />
-        <Route path="/profilepage" element={<ProfilePage />} />
-        <Route path="/settings" element={<SettingsMainPage />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Splash />} />
+          <Route path="/login1" element={<Login1 />} />
+          <Route path="/login2" element={<Login2 />} />
+          <Route path="/login3" element={<Login3 />} />
+          <Route path="/LoginA" element={<LoginA />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/profilepage" element={<ProfilePage />} />
+          <Route path="/settings" element={<SettingsMainPage />} />
+        </Routes>
+      </BrowserRouter>
+      <PostMain />
+    </div>
   );
 }
 
